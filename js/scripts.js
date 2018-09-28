5ef40b1e06478142c96607e99cbdb9817aae07fc
Jeremy Kimotho <jeremykimotho@yahoo.com>
2018-09-28 14:57:29 +0300

    I have completed the initial design of the site.

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// Business Logic
+
+
+
+// User Data
+
+$(document).ready(function () {
+  $("#start-game").click(function () {
+    $(".game-shows").toggle();
+    $(".game-hidden").toggle();
+  });
+});
