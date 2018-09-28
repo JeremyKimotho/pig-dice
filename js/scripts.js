db116bb396448e41c0a3931d22a755ba41d8494a
Jeremy Kimotho <jeremykimotho@yahoo.com>
2018-09-28 15:19:40 +0300

    I have added the start of the javascript functionality and made the 1-6 number generator

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,32 @@
 // Business Logic
 
+var numbersRoll=[1,1,1,2,2,2,3,3,3,4,4,5,5,6]
 
+var playerOne=[]
+
+var playerTwo=[]
+
+function roller(){
+  var randomNumber = numbersRoll[Math.floor(Math.random() * numbersRoll.length)];
+if(randomNumber!=1){
+  
+}
+}
 
 // User Data
+
+$(document).ready(function(){
+  $('#roll-p1').click(function(){
+    
+  })
+})
+
+$(document).ready(function () {
+  $('#roll-p2').click(function () {
+    test();
+  })
+})
+
 
 $(document).ready(function () {
   $("#start-game").click(function () {
