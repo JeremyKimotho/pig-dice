fbd04a073ea308cc2220dbaac3be69921a4a01a5
Jeremy Kimotho <jeremykimotho@yahoo.com>
2018-09-29 16:13:20 +0300

    I changed the styling to make sure of consistency, ensuring there are less than 3 colors

--- js/scripts.js
+++ js/scripts.js
@@ -20,11 +20,24 @@ function rollp1(){
     document.getElementById("roll-p1").disabled = true;
     document.getElementById("hold-p1").disabled = true;
     document.getElementById("roll-p2").disabled = false;
-    document.getElementById("roll-p2").disabled = false;
+    document.getElementById("hold-p2").disabled = false;
 
   }
 }
 
+function rollp2() {
+  var numberRolled = numbersRoll[Math.floor(Math.random() * numbersRoll.length)];
+  if (numberRolled!=1){
+    playerTwoRound.push(numberRolled)
+  } else {
+    playerTwoRound.length=0
+    document.getElementById("roll-p1").disabled = false;
+    document.getElementById("hold-p1").disabled = false;
+    document.getElementById("roll-p2").disabled = true;
+    document.getElementById("hold-p2").disabled = true;
+
+  }
+}
 
 function counter1(){
   playerOneTotal.push.apply(playerOneTotal, playerOneRound)
@@ -32,11 +45,21 @@ function counter1(){
   document.getElementById("roll-p1").disabled = true;
   document.getElementById("hold-p1").disabled = true;
   document.getElementById("roll-p2").disabled = false;
-  document.getElementById("roll-p2").disabled = false;
+  document.getElementById("hold-p2").disabled = false;
 
 }
+
 function counter2() {
-  playerTwoRound+=playerTwoTotal
+  playerTwoTotal.push.apply(playerTwoTotal, playerTwoRound)
+  playerTwoRound.length=0
+  document.getElementById("roll-p1").disabled = false;
+  document.getElementById("hold-p1").disabled = false;
+  document.getElementById("roll-p2").disabled = true;
+  document.getElementById("hold-p2").disabled = true;
+}
+
+function clearEverything(){
+  document.location.reload(true)
 }
 
 // User Data
@@ -44,28 +67,42 @@ function counter2() {
 $(document).ready(function(){
   $('#roll-p1').click(function(){
    rollp1();
-    var sum = playerOneRound.reduce(function (a, b) { return a + b; }, 0)
-    $("#round-p1").text(sum);
+    var sumP1R = playerOneRound.reduce(function (a, b) { return a + b; }, 0)
+    $("#round-p1").text(sumP1R);
   })
 })
 
 $(document).ready(function () {
   $('#roll-p2').click(function () {
-    test();
+    rollp2();
+    var sumP2R = playerTwoRound.reduce(function (a, b) { return a + b; }, 0)
+    $("#round-p2").text(sumP2R);
   })
 })
 
 $(document).ready(function () {
   $('#hold-p1').click(function () {
     counter1();
-    var sum1 = playerOneTotal.reduce(function (a, b) { return a + b; }, 0)
-    $("#total-p1").text(sum1);
+    var sumP1T = playerOneTotal.reduce(function (a, b) { return a + b; }, 0)
+    if(sumP1T<100){
+    $("#total-p1").text(sumP1T);
+    }else{
+    alert('Player One you have won the game');
+    clearEverything();
+    }
   })
 })
 
 $(document).ready(function () {
   $('#hold-p2').click(function () {
     counter2();
+    var sumP2T = playerTwoTotal.reduce(function (a, b) { return a + b; }, 0)
+    if(sumP2T<100){
+    $("#total-p2").text(sumP2T);
+    }else{
+      alert('Player Two you have won the game')
+      clearEverything()
+    }
   })
 })
 
